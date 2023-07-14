3f7a3a27d9390013fac5d10561f7bcd7958a2eb6
El hadji <mr.alioune@gmail.com>
2023-07-14 16:09:14 -0700

    int main(void)

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - is positive or negative
+ * print new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'q' && a != 'e')
+			putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
