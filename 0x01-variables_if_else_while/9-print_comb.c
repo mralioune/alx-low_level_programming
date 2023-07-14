67b7bd4b46b9c2352b6a03ecfa8a63a5948153ae
El hadji <mr.alioune@gmail.com>
2023-07-14 16:29:29 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - is ipositive or negative
+ * print new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a < 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			if (a != b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a != 56 || b != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+}
