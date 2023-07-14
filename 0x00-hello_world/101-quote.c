1e9bf67652cf83b493a3c6315cbe90ff34df3ce7
El hadji <mr.alioune@gmail.com>
2023-07-13 19:35:03 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - prints 
+ * text "nd that piece of art is useful`\" - Dora Korpar, 2015-10-19"
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
