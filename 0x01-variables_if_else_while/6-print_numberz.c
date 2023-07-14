8ebc544e437ea50869777063cccd9df5dda0d3e5
El hadji <mr.alioune@gmail.com>
2023-07-14 16:20:13 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - is ipositive or negative
+ * print new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
