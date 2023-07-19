3aa67280c1844dccd19bc16a6244a1ab4200c859
El hadji <mr.alioune@gmail.com>
2023-07-18 17:02:02 -0700

    tes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,21 +2,22 @@
 #include "stdio.h"
 
 /**
- *Write a function that prints the alphabet
- *, in lowercase, followed by a new line.
- *Prototype: void print_alphabet void
+ * print_alphabet - Write a function that prints the alphabet
+ * @alphabet: char
+ * Return: void print_alphabet void
  *You can only use _putchar twice in your code
- **/
+ */
 
 int print_alphabet(void)
 {
 	char alphabet;
 
-	alphabet = "a";
-	while (alphabet <= "z")
+	alphabet = 'a';
+	while (alphabet <= 'z')
 	{
 		_putchar(alphabet);
 		alphabet++;
 	}
 	_putchar("\n");
+	return (0);
 }
