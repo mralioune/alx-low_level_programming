5b85d916b0989581765fc548fb8d65c62568921c
El hadji <mr.alioune@gmail.com>
2023-07-18 19:10:01 -0700

    te

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign - Write a program that prints _putchar, followed by a new line
+ * @c: int  checking character
+ * Return: if c is not lower  return 0 else 1
+ */
+
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		_putchar('+')
+		return (1);
+	}
+	else if (c == 0)
+	{
+		_putchar('0')
+		return (0);
+	}
+	else
+	{
+		_putchar('-')
+		return(-1)
+	}
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - Write a program that prints _putchar, followed by a new line
+ * @c: int  checking character
+ * Return: if c is not lower  return 0 else 1
+ */
+
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (-c);
+}
