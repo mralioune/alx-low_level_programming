27ed8e68caf862c1c2b2db0f146ce514262e5190
El hadji <mr.alioune@gmail.com>
2023-07-19 15:48:17 -0700

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - computes the absolute value of an integer.
+ *
+ */
+
+void jack_bauer(void c)
+{
+	int heure;
+	int minute;
+
+	for (heure = 0 ; heure <= 23 ; heure++)
+	{
+		for (minute = 0 ; minute <= 59 ; minute++)
+		{
+			_putchar((heure / 10) + '0');
+			_putchar((heure / 10) + '0');
+			_putchar((':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute / 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
