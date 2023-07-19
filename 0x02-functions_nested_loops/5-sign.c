845385da5fe4d4b7b4b55df37e3627af5001e912
El hadji <mr.alioune@gmail.com>
2023-07-18 19:14:53 -0700

    te

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_sign - Write a program that prints _putchar, followed by a new line
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _abs - Write a program that prints _putchar, followed by a new line
