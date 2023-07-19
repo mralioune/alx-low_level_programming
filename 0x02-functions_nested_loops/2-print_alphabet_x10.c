801140e7f4b4934028204c3de9522457fd07293d
El hadji <mr.alioune@gmail.com>
2023-07-18 18:35:31 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,6 +3,8 @@
 
 /**
  * print_alphabet_x10 - Write a function that prints the alphabet
+ * @alphabet: char
+ * @    /num: char
  * Return: void print_alphabet void
  *You can only use _putchar twice in your code
  */
@@ -10,18 +12,17 @@
 int print_alphabet_x10(void)
 {
 	char alphabet;
-	int numb;
-	numb = 1;
+	int num;
 
-	alphabet = 'a';
-	while( numb <= 10)
+	for (numb = 0 ; numb <= 10 ; numb++)
 	{
+		alphabet = 'a';
 		while (alphabet <= 'z')
 		{
 			_putchar(alphabet);
 			alphabet++;
 		}
 		_putchar('\n');
-	
+	}
 	return (0);
 }
