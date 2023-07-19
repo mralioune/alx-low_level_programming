24640dadeca1f3da4c3c236a3e9959038a1640f2
El hadji <mr.alioune@gmail.com>
2023-07-19 16:41:43 -0700

    test

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* add - print 9 times table
+* @a: first number
+* @b: second number
+* starting from 0
+* followed by a new line
+* Return: sum of a and b
+*/
+
+int add(int a, int b)
+{
+	int i = a + b;
+
+	return (i);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_to_98  - print count to 98
+* @a: number input
+* followed by a new line
+* Return: countdown to 98
+*/
+
+void print_to_98(int a)
+{
+	if (a >= 98)
+	{
+		while (a >= 98)
+		{
+			printf("%d", a);
+			if (a != 98)
+				printf(", ");
+			else
+				printf("\n");
+			a++;
+		}
+	}
+	else
+	{
+		while (a <= 98)
+		{
+			printf("%d", a);
+			if (a != 98)
+				printf(", ");
+			else
+				printf("\n");
+			a++
+		}
+	}
+}
