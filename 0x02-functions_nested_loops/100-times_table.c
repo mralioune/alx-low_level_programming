5c7b0ebd85ee51e724e638df36730c3f69006775
El hadji <mr.alioune@gmail.com>
2023-07-19 17:37:24 -0700

    tes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,29 +13,41 @@ void print_times_table(int n)
 {
 	int j, m, r, k;
 
-	if (n >= 0 && n < 15)
+	if (n >= 0 && n <= 15)
 	{
 		for (j = 0; j <= n; j++)
 		{
 			for (m = 0; m <= n; m++)
 			{
-				r = j * m;
-				for (k = 100; r > 0; r = r % k)
+				k = m * j;
+
+				if (m = 0)
 				{
-					if ((r / k) > 0)
-					{
-						_putchar((r / 100) + '0');
-						k /= 10;
-					}
-					else
-						_putchar(' ');
-					_putchar((r / 10) + '0');
-					_putchar((r % 10) + '0');
+					_putchar(k + '0');
 				}
-				if (m != n)
+				else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');		
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				 else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
