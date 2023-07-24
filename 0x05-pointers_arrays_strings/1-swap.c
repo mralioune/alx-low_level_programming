478da5d48af28b0d9f6731393fe90300e3adf1d0
El hadji <mr.alioune@gmail.com>
2023-07-24 15:52:16 -0700

    1. Don't swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * swaps_int - that swaps the values of two integers.
+ * swap_int - that swaps the values of two integers.
  * @a: input parametre
  * @b: input parametre
  *  Return : n
  */
 
-void swaps_int(int *a, int *b)
+void swap_int(int *a, int *b)
 {
 	int c;
 	int d;
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen : that returns the length of a string
+ * @s: input parametre
+ * Return : s
+ */
+
+int _strlen(int *s)
+{
+	int c;
+
+	c = 0;
+	while ( *s != "\0" )
+	{
+		c ++;
+		*s++;
+	}
+
+	return (c);
+
