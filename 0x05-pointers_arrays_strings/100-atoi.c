791d1ceec92c12111eaf5871b4147c6247e13508
El hadji <mr.alioune@gmail.com>
2023-07-25 15:00:01 -0700

    val

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,11 +18,11 @@ int _atoi(char *s)
 		}
 		else if (*s >= '0' && *s <= '9')
 		{
-			num = (num * 10) + (*s - '0')
+			num = (num * 10) + (*s - '0');
 		}
 		else if (num > 0)
 		{
-			break
+			break;
 		}
 	} while (*s++)
 	return (num * sign);
