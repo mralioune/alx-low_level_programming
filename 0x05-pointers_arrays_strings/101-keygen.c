ba5dd98d8315583309b605985087e46b641935f7
El hadji <mr.alioune@gmail.com>
2023-07-25 15:26:06 -0700

    val

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - generates random passwords
+* Return: allow 0
+*/
+
+int main(void)
+{
+	int sum = 0;
+	char c;
+
+	srang(time(null));
+
+	while (sum != 2645)
+	{
+		c = rang() % 128;
+		sum += c;
+		_putchar(c);
+	}
+	_putchar(2772 - sum);
+	return (0);
+}
