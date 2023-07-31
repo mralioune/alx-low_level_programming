b096cdddc4b52e7da9f0aac03350fc0fc55a0586
El hadji <mr.alioune@gmail.com>
2023-07-31 14:24:18 -0700

    save

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memset -  bytes of the memory area pointed to by
+ * @s: constant byte
+ * @b: variable
+ * @n: varible
+ * Return: change array
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	in a = 0;
+
+	for (; n > 0; a++)
+	{
+		s[a] = b;
+		n--;
+	}
+	return (s);
+}
