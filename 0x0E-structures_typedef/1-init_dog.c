ef6268141fa009a761e124ea2cbed421ad702859
mralioune <90474203+mralioune@users.noreply.github.com>
2023-08-15 00:49:15 +0000

    Update 1-init_dog.c
    sa

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,12 +8,11 @@
  * @age: input for age
  * @owner: input gott owner
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
-	d-owner = owner;
+	d->owner = owner;
 }
