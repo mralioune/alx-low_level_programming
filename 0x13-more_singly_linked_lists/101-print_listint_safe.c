756584dbd8d4129d864d69f882132704cbd7465e
elhadji <mr.alioune@gmail.com>
2023-08-30 00:53:57 +0000

    tes

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,8 @@
 #include "lists.h"
+
 /**
- * * print_listint_safe - function that prints a linked list with a loop safely.
+ * * print_listint_safe - function that prints a
+ * linked list with a loop safely.
  * * @head: pointer to the 1st node of the linked list
  * * Return: new_node
  */
